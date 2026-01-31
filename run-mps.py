b47ce398560bb8ea800f58e38d422aa1b749f1b0
Exist-nt <teddy.bridemayor@gmail.com>
2026-01-31 05:15:44 -0500

    Add files via upload

---
+++ run-mps.py
@@ -0,0 +1,18 @@
+from ultralytics import YOLO
+import sys
+import argparse
+
+model = YOLO("visdeurbot-yolo.pt")
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("input", type=str, default="", nargs="?", help="Location of the input file")
+
+try:
+	args = parser.parse_known_args()[0]
+except:
+	print("")
+	parser.print_help()
+	sys.exit(0)
+
+model.predict(args.input, save=True, device="mps")
