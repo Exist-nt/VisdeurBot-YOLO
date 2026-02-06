5097ffef040daa564138e123bde6948ab96d812f
Exist-nt <teddy.bridemayor@gmail.com>
2026-02-06 08:32:48 -0500

    Add files via upload

--- run-cpu.py
+++ run-cpu.py
@@ -7,6 +7,7 @@ model = YOLO("visdeurbot-yolo.pt")
 parser = argparse.ArgumentParser()
 
 parser.add_argument("input", type=str, default="", nargs="?", help="Location of the input file")
+parser.add_argument("output", type=str, default="images/runs", nargs="?", help="Location of the output folder")
 
 try:
 	args = parser.parse_known_args()[0]
@@ -15,4 +16,4 @@ except:
 	parser.print_help()
 	sys.exit(0)
 
-model.predict(args.input, save=True, device=cpu, save_dir="images/runs")
+model.predict(args.input, save=True, device=cpu, save_dir=args.output)
